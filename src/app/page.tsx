5bec9566b7f462654a0c68cc8fcc662a00e55395
nucasspro <hailongdang.hailong@gmail.com>
2023-10-09 00:46:04 +0700

    fix: build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import { InputForm } from "@/components/app-components/user-input";
+import Image from "next/image";
+import { use, useEffect, useState } from "react";
 
 export default function Home() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Button</Button>
+      {isClient && <InputForm />}
     </main>
   );
 }
